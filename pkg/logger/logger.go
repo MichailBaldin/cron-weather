639056f7526be1a3fd67ab1d00ffd7fe22ccd0b1
Mick <michailbaldin@gmail.com>
2026-02-17 12:28:38 +0300

    feat(infra): add log to file

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -5,7 +5,10 @@ import (
 	"os"
 )
 
-const envLocal = "local"
+const (
+	envLocal = "local"
+	envProd  = "prod"
+)
 
 func SetupLogger(env string) *slog.Logger {
 	var log *slog.Logger
@@ -13,6 +16,13 @@ func SetupLogger(env string) *slog.Logger {
 	switch env {
 	case envLocal:
 		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o666)
+		if err != nil {
+			log.Error("failed to create telegram sender", "error", err)
+			os.Exit(1)
+		}
+		log = slog.New(slog.NewJSONHandler(file, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	default:
 		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	}
