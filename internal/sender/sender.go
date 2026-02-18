ab6fc2d90d44824e2789cf4295e94188114278a0
Mick <michailbaldin@gmail.com>
2026-02-18 11:33:29 +0300

    feat(sender): add split message by limit for tg

--- internal/sender/sender.go
+++ internal/sender/sender.go
@@ -1,7 +1,10 @@
+// Package sender defines message delivery interfaces and implementations
 package sender
 
 import "context"
 
+// Sender delivers a batch of alert messages to some destination.
+// Implementations should treat an empty batch as a no-op.
 type Sender interface {
 	Send(ctx context.Context, messages []string) error
 }
